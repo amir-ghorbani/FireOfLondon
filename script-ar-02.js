01c0db9895bb0ccbfd4221e993bf45383aba0059
amir-ghorbani <amir.ghorbani.design@gmail.com>
2022-03-07 21:08:20 +0000

    Add files via upload

---
+++ script-ar-02.js
@@ -0,0 +1,29 @@
+document.querySelector('.tower')
+.addEventListener('mouseenter', e => {
+    location.href = "tower.html";
+});
+
+document.querySelector('.seething-lane')
+.addEventListener('mouseenter', e => {
+    location.href = "seething-lane.html";
+});
+
+document.querySelector('.royal-exchange')
+.addEventListener('mouseenter', e => {
+    location.href = "royal-exchange.html";
+});
+
+document.querySelector('.st-paul')
+.addEventListener('mouseenter', e => {
+    location.href = "st-paul.html";
+});
+
+document.querySelector('.whitehall')
+.addEventListener('mouseenter', e => {
+    location.href = "whitehall.html";
+});
+
+document.querySelector('.london-before-map')
+.addEventListener('mouseenter', e => {
+    location.href = "london-before-map.html";
+});
---
+++ script-ar-03.js
@@ -0,0 +1,9 @@
+document.querySelector('.seething-lane')
+.addEventListener('mouseenter', e => {
+    location.href = "seething-lane.html";
+});
+
+document.querySelector('.pudding-lane')
+.addEventListener('mouseenter', e => {
+    location.href = "pudding-lane.html";
+});
